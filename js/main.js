974668e6b77ad27be629ab178d653918f705cf09
Nazmul <nazmulhasanfci@gmail.com>
2021-08-06 19:51:02 +0600

    add back to top arrow

--- js/main.js
+++ js/main.js
@@ -110,9 +110,6 @@
 
 
 
-
-
-
 	// scrollToTop
 	$.scrollUp({
 		scrollName: 'scrollUp', // Element ID
@@ -121,7 +118,7 @@
 		animation: 'fade', // Fade, slide, none
 		animationInSpeed: 200, // Animation in speed (ms)
 		animationOutSpeed: 200, // Animation out speed (ms)
-		scrollText: '<i class="icofont icofont-long-arrow-up"></i>', // Text for element
+		scrollText: '<i class="far fa-hand-point-up"></i>', // Text for element
 		activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
 	});
 
